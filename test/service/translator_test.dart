c5239d79386e93420093bc46173cda153c771b25
Gustavo duarte <gustavohd18@gmail.com>
2020-09-26 11:36:26 -0300

    add test to translator

--- test/service/translator_test.dart
+++ test/service/translator_test.dart
@@ -1,16 +1,27 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:myFindMovies/service/translator/languages.dart';
+import 'package:myFindMovies/service/translator/translator.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
-  test('Can Create Preferences', () async {
+  test('Should be set language correct', () async {
     SharedPreferences.setMockInitialValues({}); //set values here
     SharedPreferences pref = await SharedPreferences.getInstance();
-    bool working = false;
-    String name = 'john';
-    pref.setBool('working', working);
-    pref.setString('name', name);
+    var translator = Translator();
 
-    expect(pref.getBool('working'), false);
-    expect(pref.getString('name'), 'john');
+    translator.setLanguage(Languages.english);
+    var result = await translator.getLanguage();
+
+    expect(result, Languages.english);
+
+    translator.setLanguage(Languages.portuguese);
+    var result2 = await translator.getLanguage();
+
+    expect(result2, Languages.portuguese);
+
+    translator.setLanguage(Languages.spanish);
+    var result3 = await translator.getLanguage();
+
+    expect(result3, Languages.spanish);
   });
 }
