4a0a980f816220e5be3242c3bf53f13bd55c70d0
Gustavo duarte <gustavohd18@gmail.com>
2020-10-05 22:10:23 -0300

    add login translator

--- lib/stores/login/login_controller.dart
+++ lib/stores/login/login_controller.dart
@@ -1,12 +1,21 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:mobx/mobx.dart';
 import 'package:myFindMovies/service/authentication/authentification_abstract.dart';
+import 'package:myFindMovies/service/translator/languages.dart';
+import 'package:myFindMovies/service/translator/translator.dart';
 part 'login_controller.g.dart';
 
 class LoginController = _LoginControllerBase with _$LoginController;
 
 abstract class _LoginControllerBase with Store {
   final Authentication _authentication = Modular.get();
+  final _translator = Modular.get<Translator>();
+  String _placeholder;
+  String _placeholderPassword;
+
+  _LoginControllerBase() {
+    _getTranslator();
+  }
 
   @observable
   String email;
@@ -28,6 +37,29 @@ abstract class _LoginControllerBase with Store {
 
   @observable
   bool enableButton = false;
+
+  @observable
+  String passwordWord = "Password";
+  @observable
+  String cancelButton = "Cancel";
+  @observable
+  String errorLoginMessage = "Error with login: Password or email invalid";
+  @observable
+  String resetPasswordMessage =
+      "To reset password, please type your email and follow instruction sent to email";
+
+  @observable
+  String resetPasswordTitle = "Reset password";
+  @observable
+  String createAccountText = "Create Account";
+
+  @observable
+  String sendButton = "Send";
+  @observable
+  String sendButtonOk = "Email sent";
+
+  @observable
+  String errorSendReset = "Email not send try again";
 
   void setEmail(String emailSend) {
     email = emailSend;
@@ -116,7 +148,7 @@ abstract class _LoginControllerBase with Store {
       emailPlaceholder = "";
       return true;
     } else if (email.length > 0) {
-      emailPlaceholder = 'Enter a valid email';
+      emailPlaceholder = _placeholder;
       return false;
     } else {
       return false;
@@ -130,10 +162,60 @@ abstract class _LoginControllerBase with Store {
       passwordPlaceholder = "";
       return true;
     } else if (password.length > 0) {
-      passwordPlaceholder = 'Password needs to be at least 6 characters';
+      passwordPlaceholder = _placeholderPassword;
       return false;
     } else {
       return false;
     }
+  }
+
+  void _getTranslator() async {
+    var result = await _translator.getLanguage();
+
+    if (result == Languages.english) {
+      passwordWord = "Password";
+      cancelButton = "Cancel";
+      errorLoginMessage = "Error with login: Password or email invalid";
+      resetPasswordMessage =
+          "To reset password, please type your email and follow instruction sent to email";
+      resetPasswordTitle = "Reset password";
+      createAccountText = "Create Account";
+      sendButton = "Send";
+      sendButtonOk = "Email sent";
+      errorSendReset = "Email not send try again";
+      _placeholder = "Enter a valid email";
+      _placeholderPassword = 'Password needs to be at least 6 characters';
+    } else if (result == Languages.portuguese) {
+      passwordWord = "Senha";
+      errorLoginMessage = "Erro com o login: Senha ou Email invalido";
+      resetPasswordMessage =
+          "Para redefinir a senha, por favor digite seu email e siga as instruções enviadas para o Email";
+      resetPasswordTitle = "Redefinir a senha";
+      createAccountText = "Criar uma conta";
+      sendButton = "Enviar";
+      cancelButton = "Cancelar";
+      sendButtonOk = "Email enviado";
+      errorSendReset = "Email não enviado, tente novamente";
+      _placeholder = "Entre com um email valido";
+      _placeholderPassword = 'Senha precisa ter mais de 6 digitos';
+    } else {
+      passwordWord = "contraseña";
+      errorLoginMessage =
+          "Error de inicio de sesión: contraseña o correo electrónico no válidos";
+      resetPasswordMessage =
+          "Para restablecer la contraseña, ingrese su correo electrónico y siga las instrucciones enviadas a Correo electrónico";
+      resetPasswordTitle = "Restablecer la contraseña";
+      createAccountText = "Crear una cuenta";
+      sendButton = "Enviar";
+      cancelButton = "Cancelar";
+      sendButtonOk = "Correo electrónico enviado";
+      errorSendReset = "Correo electrónico no enviado, inténtalo de nuevo";
+      _placeholder = "Introduzca una dirección de correo electrónico válida";
+      _placeholderPassword = 'La contraseña debe tener más de 6 dígitos';
+    }
+  }
+
+  void reload() async {
+    _getTranslator();
   }
 }
