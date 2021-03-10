fb61a47d0b626a240a7e3a027d5fe49ab9616dff
gustavohd18 <gustavo.hernandez@acad.pucrs.br>
2021-03-10 20:37:53 -0300

    add menu

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,45 +1,29 @@
-import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:my_find_series_and_movies/widgets/cardInformation/card_infomation.dart';
 
-//CardInformation(urlImage: "https://lumiere-a.akamaihd.net/v1/images/bigred_ka_vert_brpo_cddc98da.jpeg", color: Colors.blue[300], rate: "10")
+import 'package:flutter/material.dart';
+import 'package:my_find_series_and_movies/util/constants.dart';
+import 'package:my_find_series_and_movies/widgets/BodyMenu/body_menu.dart';
+
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          "My find Series And movies",
-          style: GoogleFonts.pacifico(fontSize: 25),
-        ),
-      ),
-      body: Column(
-        children: [
-          Padding(
-            padding: EdgeInsets.all(10),
-            child: Text(
-              "Top 10 popular Movies",
-              style: TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-                shadows: [
-                  Shadow(
-                    blurRadius: 10.0,
-                    color: Colors.black.withOpacity(0.5),
-                    offset: Offset(5.0, 5.0),
-                  ),
-                ],
-              ),
-            ),
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: IconButton(
+          padding: EdgeInsets.only(left: kDefaultPadding),
+          icon: Icon(Icons.menu, color: Colors.black,),
+          onPressed: ()=> print("hello"),
           ),
-          Column(
-            children:<Widget> [
-
-            ],
-          )
-        ],
-      ),
+          actions:<Widget> [
+            IconButton(
+              icon: Icon(Icons.search, color: Colors.black,),
+              padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+               onPressed: () => print("oii")
+            )
+          ],
+        ),
+        body: BodyMenu(),
     );
   }
 }
---
+++ lib/widgets/BodyMenu/body_menu.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:my_find_series_and_movies/util/constants.dart';
+
+class BodyMenu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Category();
+  }
+}
+
+class Category extends StatefulWidget {
+  @override
+  _CategoryState createState() => _CategoryState();
+}
+
+class _CategoryState extends State<Category> {
+  int selectedCategory = 1;
+  List<String> categories = ["Top popular movies", "Top popular tv shows"];
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: categories.length,
+        itemBuilder: (context, index) => buildCategory(index, context),
+      ),
+    );
+  }
+
+  Padding buildCategory(int index, BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+      child: GestureDetector(
+        onTap: () {
+          setState(() {
+            selectedCategory = index;
+          });
+        },
+        child: Column(
+          children: [
+            Text(
+              categories[index],
+              style: Theme.of(context).textTheme.headline5.copyWith(
+                  fontWeight: FontWeight.w600,
+                  color: index == selectedCategory
+                      ? kTextColor
+                      : Colors.black.withOpacity(0.4)),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: kDefaultPadding / 2),
+              height: 6,
+              width: 150,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: index == selectedCategory
+                      ? kSecondaryColor
+                      : Colors.transparent),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
